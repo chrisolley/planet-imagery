a1adb40cce1dde464a8dedf0e83b671e3099d374
Ubuntu <ubuntu@ip-172-31-35-15.us-west-2.compute.internal>
2019-07-29 20:05:17 +0000

    update extract data script

--- extract_data.sh
+++ extract_data.sh
@@ -1,13 +1,16 @@
-7za x data/train-jpg.tar.7z
-tar xf data/train-jpg.tar
+kaggle competitions download -p ./data -c planet-understanding-the-amazon-from-space
+sudo apt-get install p7zip-full
+sudo apt-get install unzip
+7za x data/train-jpg.tar.7z -o./data
+tar xf data/train-jpg.tar -C ./data
 rm data/train-jpg.tar*
-7za x data/test-jpg.tar.7z
-tar xf data/test-jpg.tar
+7za x data/test-jpg.tar.7z -o./data
+tar xf data/test-jpg.tar -C ./data
 rm data/test-jpg.tar*
-7za x data/test-jpg-additional.tar.7z
-tar xf data/test-jpg-additional.tar
+7za x data/test-jpg-additional.tar.7z -o./data
+tar xf data/test-jpg-additional.tar -C ./data
 rm data/test-jpg-additional.tar*
-unzip data/sample_submission_v2.csv.zip
-unzip data/train_v2.csv.zip
+unzip data/sample_submission_v2.csv.zip -d ./data
+unzip data/train_v2.csv.zip -d ./data
 rm data/sample_submission_v2.csv.zip
 rm data/train_v2.csv.zip
